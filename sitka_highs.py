df3feeef9841a88f65cadf75aada0d7bfcdbe2df
Arte <jiangtao.cheng@orderplus.com>
2021-09-01 16:51:44 +0800

    2018年每日最高温度

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,8 +1,9 @@
 import csv
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -11,9 +12,11 @@ with open(filename) as f:
         print(index, column_header)
 
     # 从文件中获取最高温度。
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
 print(highs)
@@ -21,11 +24,12 @@ print(highs)
 # 根据最高温度绘制图形。
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
 # 设置图形的格式。
-ax.set_title('2018年7月每日最高温度', fontsize=24)
+ax.set_title('2018年每日最高温度', fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel('温度 (F)', fontsize=16)
 ax.tick_params(axis='both', which='major', labelsize=16)
 
